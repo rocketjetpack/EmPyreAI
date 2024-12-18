b31bce9973c249062ede6d71cdc4c2782665be3d
Kali McLennan <kmclennan@flatironinstitute.org>
2024-12-18 12:10:11 -0500

    Begin creation of Slurm REST API client class.

---
+++ src/EmPyreAI/EmpireSlurm.py
@@ -0,0 +1,40 @@
+# This file contains the EmpireSlurm class to represent a group of users on the Empire AI Alpha system.
+#
+# Class Properties (readonly):
+#   - id = (get) Returns the GID number
+#   - name = (get) Returns the name of the groups
+#   - members = (get) Returns a list of usernames with membership in this group
+#
+# Static Functions:
+#   - Exists(): Returns bool. True if the group exists, False if it does not.
+#
+# Class Functions:
+#   - GetFromCMD(): Loads group data from the Base Command API into the group_data variable. Returns (bool)
+#   - Commit(): Commits changes of group information to the Base Command API. Returns (bool).
+#   - AddMember(): Adds a member to the membership list of this group. Returns (bool).
+#   - RemoveMember(): Removes a member from the membership list of this group. Returns (bool).
+#  
+# This makes use of the EmPyreAI module and ultimately the Base Command API
+#   to provide user management capabilities to coordinators without requiring
+#   elevated privileges on the cluster.
+#
+# API Documentation: https://slurm.schedmd.com/rest_api.html
+#
+# Author: Kali McLennan (Flatiron Institute) - kmclennan@flatironinstitute.org
+
+class EmpireSlurm:
+    config = {
+        "apiVersion": "v0.0.39",
+        "apiServer": "alpha-mgr",
+        "protocol": "http",
+        "port": 6820,
+    }
+
+    endPoints = {
+        "diag": "slurm/" + self.config["apiVersion"] + "/diag",
+        "accounts": "slurmdb/" + self.config["apiVersion"] + "/accounts",
+        "account": "slurmdb/" + self.config["apiVersion"] + "/account/_NAME",
+        "partitions": "slurm/" + self.config["apiVersion"] + "/partitions",
+        "partition": "slurm/" + self.config["apiVersion"] + "/partition/_NAME",
+    },
+
