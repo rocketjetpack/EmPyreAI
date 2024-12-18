a3c02b78cda79dbc63728d410f2d403f61f28722
Kali McLennan <kmclennan@flatironinstitute.org>
2024-12-18 13:12:37 -0500

    Prototype of GET functionality ready for testing.

--- src/EmPyreAI/EmpireSlurm.py
+++ src/EmPyreAI/EmpireSlurm.py
@@ -22,6 +22,12 @@
 #
 # Author: Kali McLennan (Flatiron Institute) - kmclennan@flatironinstitute.org
 
+import getpass
+import requests
+import os
+import sys
+
+
 class EmpireSlurm:
     config = {
         "apiVersion": "v0.0.39",
@@ -34,8 +40,38 @@ class EmpireSlurm:
         self.endPoints = {
             "diag": "slurm/" + self.config["apiVersion"] + "/diag",
             "accounts": "slurmdb/" + self.config["apiVersion"] + "/accounts",
-            "account": "slurmdb/" + self.config["apiVersion"] + "/account/_NAME",
+            "account": "slurmdb/" + self.config["apiVersion"] + "/account/",
             "partitions": "slurm/" + self.config["apiVersion"] + "/partitions",
-            "partition": "slurm/" + self.config["apiVersion"] + "/partition/_NAME",
-        },
+            "partition": "slurm/" + self.config["apiVersion"] + "/partition/",
+        }
+        self.username = getpass.getuser()
 
+    #region Headers
+    # Headers need to include X-SLURM-USER-NAME and X-SLURM-USER-TOKEN
+    def GetHeaders(self):
+        return {
+            "X-SLURM-USER-NAME": self.username,
+            "X-SLURM-USER-TOKEN": self.token
+        }
+    #endregion
+
+    #region Token Property
+    def GetToken(self):
+        return self.jwt
+    
+    def SetToken(self, value):
+        self.jwt = value    
+
+    token = property(GetToken, SetToken)
+    #endregion
+
+    def Post(self):
+        pass
+
+    def Get(self):
+        url = f"{self.config['protocol']}://{self.config['apiServer']}:{self.config['port']}/{self.endPoints['diag']}
+        response = requests.get(api_url, headers=self.GetHeaders())
+        print(response)
+
+    def Put(self):
+        pass
