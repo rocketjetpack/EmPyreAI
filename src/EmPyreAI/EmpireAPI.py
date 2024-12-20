bf3cbd7069ddbc1eeac4b25700083c71a527a7c9
Kali McLennan <kmclennan@flatironinstitute.org>
2024-12-20 16:25:20 -0500

    Start branching the cluster object to a singleton module file.

---
+++ src/EmPyreAI/EmpireAPI.py
@@ -0,0 +1,17 @@
+import getpass
+from pythoncm.cluster import Cluster
+from pythoncm.settings import Settings
+
+CMSH_Cluster = None
+
+if getpass.getuser() != "root":
+     settings = Settings(
+        host="alpha-mgr",
+        port=8081,
+        cert_file=f'/mnt/home/{getpass.getuser()}/.empireai/cmsh_api.pem',
+        key_file=f'/mnt/home/{getpass.getuser()}/.empireai/cmsh_api.key',
+        ca_file='/usr/lib64/python3.9/site-packages/pythoncm/etc/cacert.pem'
+     )
+     CMSH_Cluster = Cluster(settings)
+else:
+     CMSH_Cluster = Cluster()
